a3614d1ef7cd019069863b1dd59f06e49b28a088
Eric Li <ericlmtn@gmail.com>
2025-11-30 20:18:28 -0500

    Fix build errors

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ export default function About() {
 			<div className="max-w-4xl 2xl:max-w-5xl 3xl:max-w-6xl mx-auto px-3 sm:px-4 md:px-6 2xl:px-8">
 				<h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl 3xl:text-7xl font-bold mb-3 sm:mb-4 xl:mb-6 2xl:mb-8">About Luminary</h2>
 				<p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl 3xl:text-5xl">
-					We strive to make navigating UNC's campus easier and more accessible for all.
+					We strive to make navigating UNC&apos;s campus easier and more accessible for all.
 					Our mission is to develop an inclusive, user-friendly tool that helps
 					individuals find personalized routes tailored to their mobility needs. Together,
 					we can create a more connected and barrier-free campus experience with Luminary.
--- src/components/Accessible.tsx
+++ src/components/Accessible.tsx
@@ -13,7 +13,6 @@ export default function Accessible() {
 							width={1264}
 							height={580}
 							className="w-full h-auto"
-							className="w-full h-auto"
 						/>
 					</div>
 				</div>
